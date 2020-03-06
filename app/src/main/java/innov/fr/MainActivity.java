0bcfe38e3647a4fdc8971f17bedef8a606eb6a64
vincedollo <vincent.dollo@hotmail.fr>
2020-03-06 11:22:33 +0100

    test

--- app/src/main/java/innov/fr/MainActivity.java
+++ app/src/main/java/innov/fr/MainActivity.java
@@ -1,25 +1,35 @@
 package innov.fr;
+import android.content.Intent;
+import android.media.Image;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button bouton;
+    private ImageView click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity);
 
-        Button bt = (Button) findViewById(R.id.button);
-        bt.setOnClickListener(new View.OnClickListener() {
+        this.click=(ImageView) findViewById(R.id.click);
+
+        click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent otherActivity = new Intent(getApplicationContext(), ActivityTest.class);
+                startActivity(otherActivity);
+                finish();
             }
         });
+
+
     }
 }
