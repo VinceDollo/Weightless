b6725e7d96f8c93bb4965a1cf4210f17294bec80
vincedollo <vincent.dollo@hotmail.fr>
2020-03-13 11:42:07 +0100

    10000

---
+++ app/src/main/java/innov/fr/cliquesActivity.java
@@ -0,0 +1,14 @@
+package innov.fr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cliquesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cliquer_activite);
+    }
+}
