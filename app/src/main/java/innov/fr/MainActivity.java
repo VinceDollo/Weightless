60eeb1efa64d8d8e87cbfd9e368b66c783a871cd
AlanOnym <alan-peron@hotmail.com>
2020-03-21 18:35:58 +0100

    Suppression d'une vingtaine de warnings Ajout des activités connexion, résults, settings
    modification d'une ligne
    android:screenOrientation="portrait" ->
    android:screenOrientation="fullSensor"
    car elle causait une erreur

--- app/src/main/java/innov/fr/MainActivity.java
+++ app/src/main/java/innov/fr/MainActivity.java
@@ -1,16 +1,10 @@
 
 package innov.fr;
 import android.content.Intent;
-import android.media.Image;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import android.os.Bundle;
 
@@ -20,16 +14,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.btn1);
+        button = findViewById(R.id.btn_weight);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivity2();
+                openActivityWeight();
+            }
+        });
+
+        button = findViewById(R.id.btn_results);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityResults();
+            }
+        });
+
+        button = findViewById(R.id.btn_settings);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivitySettings();
+            }
+        });
+
+        button = findViewById(R.id.btn_connexion);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityConnexion();
             }
         });
     }
-    public void openActivity2() {
+    public void openActivityWeight() {
         Intent intent = new Intent(this, NewActivityTest.class);
+        startActivity(intent);
+    }
+    public void openActivityResults() {
+        Intent intent = new Intent(this, activity_results.class);
+        startActivity(intent);
+    }
+    public void openActivitySettings() {
+        Intent intent = new Intent(this, activity_settings.class);
+        startActivity(intent);
+    }
+    public void openActivityConnexion() {
+        Intent intent = new Intent(this, activity_connexion.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/innov/fr/activity_results.java
@@ -0,0 +1,14 @@
+package innov.fr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_results extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}
