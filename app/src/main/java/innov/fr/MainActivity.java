9c38a0f630d6b29114ed54304652d8f767988bd9
mezianekhodja <meziane.khodja@etudiant.univ-rennes1.fr>
2020-10-16 19:03:22 +0200

    page d'acceuil

--- app/src/main/java/innov/fr/MainActivity.java
+++ app/src/main/java/innov/fr/MainActivity.java
@@ -2,30 +2,36 @@
 package innov.fr;
 import android.content.Intent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
-
+    private int SLEEP_TIMER=3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
-        button = findViewById(R.id.buttonStart);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivityConnexion();
-            }
-        });
-
+        getSupportActionBar().hide();
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
     }
-    public void openActivityConnexion() {
-        Intent intent = new Intent(this, activity_connexion.class);
-        startActivity(intent);
+    private class LogoLauncher extends Thread{
+        public void run(){
+            try{
+                sleep(1000*SLEEP_TIMER);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            Intent intent = new Intent(MainActivity.this, activity_connexion.class);
+            startActivity(intent);
+            MainActivity.this.finish();
+        }
     }
 }
 
