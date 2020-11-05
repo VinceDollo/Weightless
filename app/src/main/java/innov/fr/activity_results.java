1ebdf9e376a21ce3e1833a00889fa6ffabc689bc
mezianekhodja <meziane.khodja@etudiant.univ-rennes1.fr>
2020-11-05 12:05:47 +0100

    recuperation db

--- app/src/main/java/innov/fr/activity_results.java
+++ app/src/main/java/innov/fr/activity_results.java
@@ -1,5 +1,6 @@
 package innov.fr;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -8,12 +9,26 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class activity_results extends AppCompatActivity {
     private Button button;
     private FirebaseAuth firebaseAuth;
+    private TextView tvres;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference noteRef =  db.collection("meziane").document("poubellen0");
+    private static final String TAG = "activity_results";
+    private static final String KEY_DAY = "day", KEY_MONTH = "month", KEY_POIDS="poids", KEY_YEAR="year";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +63,10 @@ public class activity_results extends AppCompatActivity {
                 openActivityProfil();
             }
         });
+
+        button = findViewById(R.id.buttonresultsGO);
+
+        tvres = findViewById(R.id.tvresults);
 
         firebaseAuth=FirebaseAuth.getInstance();
     }
@@ -84,6 +103,32 @@ public class activity_results extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
         return true;
+    }
+    public void loadNote(View v){
+        noteRef.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()){
+                            String day = documentSnapshot.getString(KEY_DAY);
+                            String month = documentSnapshot.getString(KEY_MONTH);
+                            String year = documentSnapshot.getString(KEY_YEAR);
+                            String poids = documentSnapshot.getString(KEY_POIDS);
+
+                            tvres.setText("Date = "+day+"/"+month+"/"+year+", poids = "+poids+"kg");
+                        }
+                        else{
+                            Toast.makeText(activity_results.this, "Fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(activity_results.this, "Fail", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
     }
 
     @Override
