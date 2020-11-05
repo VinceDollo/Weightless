c14032f6248007aa33c0140159988ec0e9e79a7a
mezianekhodja <meziane.khodja@etudiant.univ-rennes1.fr>
2020-11-05 10:47:00 +0100

    ajout mesure

--- app/src/main/java/innov/fr/activity_weight.java
+++ app/src/main/java/innov/fr/activity_weight.java
@@ -1,5 +1,6 @@
 package innov.fr;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -8,12 +9,25 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class activity_weight extends AppCompatActivity {
     private Button button;
     private FirebaseAuth firebaseAuth;
+    private static final String TAG = "activity_weight";
+    private EditText etuser, etdate,etpoids;
+    private static final String KEY_USER = "user", KEY_DATE = "date", KEY_POIDS="poids";
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,8 +65,41 @@ public class activity_weight extends AppCompatActivity {
             }
         });
 
+        etuser = findViewById(R.id.etwheightuser);
+        etdate = findViewById(R.id.etwheightdate);
+        etpoids = findViewById(R.id.etwheightpoids );
+
+        button = findViewById(R.id.buttonweightGO);
+
         firebaseAuth = FirebaseAuth.getInstance();
     }
+
+    public void saveNote(View v) {
+        String user = etuser.getText().toString();
+        String date = etdate.getText().toString();
+        String poids = etpoids .getText().toString();
+
+        Map<String, Object> note = new HashMap<>();
+        note.put(KEY_USER,user);
+        note.put(KEY_DATE,date);
+        note.put(KEY_POIDS,poids );
+
+        db.collection("Notebook").document("My First Note").set(note)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(activity_weight.this, "Sucess", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(activity_weight.this, "Fail", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
+    }
+
     public void openActivityWeight() {
         finish();
         Intent intent = new Intent(this, activity_weight.class);
