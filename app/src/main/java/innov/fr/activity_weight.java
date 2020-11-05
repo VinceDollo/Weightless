36780b8662e102eeb37cd5a202d93d6bf3ac0094
mezianekhodja <meziane.khodja@etudiant.univ-rennes1.fr>
2020-11-05 11:18:52 +0100

    mise en forme db

--- app/src/main/java/innov/fr/activity_weight.java
+++ app/src/main/java/innov/fr/activity_weight.java
@@ -15,6 +15,12 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -29,8 +35,10 @@ public class activity_weight extends AppCompatActivity {
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private int poubelleCompteur=0;
-    private String username = "Mez";
+    private String username = "Undefined";
     private String namePoubelle = "poubellen"+String.valueOf(poubelleCompteur);
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,7 +83,25 @@ public class activity_weight extends AppCompatActivity {
 
         button = findViewById(R.id.buttonweightGO);
 
+        firebaseDatabase = FirebaseDatabase.getInstance();
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+
+        DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserProfile userProfile = snapshot.getValue(UserProfile.class);
+                username =userProfile.getUserName();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(activity_weight.this, error.getCode(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void saveNote(View v) {
