87ebcfb949137a70b33e58fb1b479cc6aa97f99e
sazagui <saoussane20@gmail.com>
2021-03-18 01:10:36 +0100

    Client de config de ESP32

---
+++ app/src/main/java/innov/fr/Shared.java
@@ -0,0 +1,29 @@
+package innov.fr;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteOrder;
+
+public class Shared {
+    public static String getIpAddress(Context context){
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
+        if (ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN)) {
+            ipAddress = Integer.reverseBytes(ipAddress);
+        }
+        byte[] ipByteArray = BigInteger.valueOf(ipAddress).toByteArray();
+        String ipAddressString;
+        try {
+            ipAddressString = InetAddress.getByAddress(ipByteArray).getHostAddress();
+        } catch (UnknownHostException ex) {
+            Log.e("WIFIIP", "Unable to get host address.");
+            ipAddressString = null;
+        }
+        return ipAddressString;
+    }
+}
--- app/src/main/java/innov/fr/activity_settings_balance.java
+++ app/src/main/java/innov/fr/activity_settings_balance.java
@@ -1,7 +1,9 @@
 package innov.fr;
 
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,9 +15,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+
 public class activity_settings_balance extends AppCompatActivity {
 
     private Button button;
+    private Button btnSend;
     private EditText nomWifi;
     private EditText mdpWifi;
     private FirebaseAuth firebaseAuth;
@@ -62,7 +84,56 @@ public class activity_settings_balance extends AppCompatActivity {
         nomWifi = findViewById(R.id.textwifi);
         mdpWifi = findViewById(R.id.textWifi);
 
-        button = findViewById(R.id.btnwifi);
+        btnSend = findViewById(R.id.btnwifi);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(){
+                    public void run(){
+                        String ip = Shared.getIpAddress(getApplicationContext());
+                        //System.out.println("My ip addresss is : " + ip);
+                        String[] temp = ip.split("\\.");
+                        String target = temp[0]+"."+temp[1]+"."+temp[2]+".1";
+                        //System.out.println(target);
+                        JSONObject object = new JSONObject();
+                        try {
+                            object.put("ssid", nomWifi.getText().toString());
+                            object.put("passWiFi", mdpWifi.getText().toString());
+                            object.put("nameAp", "ere");
+                            object.put("passAp", "rr");
+
+                            System.out.println(object);
+
+                            ByteBuffer buffer = ByteBuffer.allocate(4096);
+                            buffer.clear();
+                            Charset charset = Charset.forName("UTF-8");
+                            SocketChannel client = SocketChannel.open();
+                            client.connect(new InetSocketAddress(target, 52000));
+                            client.write(charset.encode(String.valueOf(object)));
+
+                            while(true)
+                                if(client.read(buffer)>0){
+                                    buffer.flip();
+                                    String res = new String(buffer.array());
+                                    //System.out.println(res);
+                                    buffer.clear();
+                                    break;
+                                }
+                            client.close();
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        catch (UnknownHostException e){
+                            System.out.println("Target is unrecheable");
+                            e.printStackTrace();
+                        }catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+            }
+        });
 
     }
 
